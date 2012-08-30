fdb6e1d604c641ad573dcf71a1d22d0ff606c963
Justin Bull <me@justinbull.ca>
2012-08-30 17:23:45 -0400

    Moved "WIP" page into Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vagrant::Application.config.secret_token = '653479fd8823e3f3e5ce66e45b1dc02e91952d8703c74ea3c93396ec884c34abf78837af1660e569bea5539f7604946fd98d9c45d1dc91696688bf2f9c3451cd'
